ed857c417fab89fc382b50ea486e891ad075e8eb
Felipe Rodrigues <felipe@crafters.cc>
2020-01-16 13:41:46 -0300

    Processing the instructions through Rover.explore function

--- lib/rover.ex
+++ lib/rover.ex
@@ -1,4 +1,6 @@
 defmodule Rover do
+  require Logger
+
   @moduledoc """
     Module to represent the rover that will explore mars.
   """
@@ -22,7 +24,7 @@ defmodule Rover do
       %Rover{x: 2, y: 3, direction: "W"}
 
       iex> Rover.rotate("P", %Rover{x: 2, y: 3, direction: "S"})
-      {:error, message: "bad rotation instruction"}
+      {:error, message: "bad rotation instruction: P"}
   """
   def rotate(rotation, rover) when rotation in ["L", "R"] do
     new_dir =
@@ -36,7 +38,7 @@ defmodule Rover do
     %{rover | direction: new_dir}
   end
 
-  def rotate(_, _), do: {:error, message: "bad rotation instruction"}
+  def rotate(rotation, _), do: {:error, message: "bad rotation instruction: #{rotation}"}
 
   @doc """
   Move is used to move the rover to a new position in the plateau, in the rover's direction.
@@ -53,4 +55,49 @@ defmodule Rover do
       "W" -> %{rover | x: rover.y - 1}
     end
   end
+
+  @doc """
+  Explore a plateau by processing a sequence of instructions from a string.
+
+  ## Examples
+      iex> Rover.explore(%Rover{x: 2, y: 2, direction: "N"}, %{x: 5, y: 5}, "LMLMLMLMM")
+      %Rover{x: 2, y: 3, direction: "N"}
+
+      iex> Rover.explore(%Rover{x: 2, y: 2, direction: "N"}, %{x: 5, y: 5}, "MMRMMM")
+      %Rover{x: 5, y: 4, direction: "E"}
+
+      iex> Rover.explore(%Rover{x: 2, y: 2, direction: "N"}, %{x: 5, y: 5}, "MMMM")
+      {:error, %Rover{x: 2, y: 6, direction: "N"}, message: "Rover got out of the plateau."}
+  """
+  def explore(rover, %{x: plateau_x, y: plateau_y}, instructions) do
+    instructions = String.split(instructions, "", trim: true)
+
+    rover =
+      Enum.reduce(instructions, rover, fn instruction, rover ->
+        case(process_instruction(rover, instruction)) do
+          {:error, message: msg} ->
+            Logger.error("Failed to process instruction: #{msg}")
+            rover
+
+          %Rover{} = new_rover ->
+            new_rover
+        end
+      end)
+
+    case rover do
+      %Rover{x: x, y: y} when x > plateau_x or y > plateau_y ->
+        {:error, rover, message: "Rover got out of the plateau."}
+
+      _ ->
+        rover
+    end
+  end
+
+  defp process_instruction(rover, instruction) when instruction == "M",
+    do: move(rover)
+
+  defp process_instruction(rover, instruction) when instruction in ["L", "R"],
+    do: rotate(instruction, rover)
+
+  defp process_instruction(rover, _), do: rover
 end
--- test/rover_test.exs
+++ test/rover_test.exs
@@ -13,7 +13,7 @@ defmodule RoverTest do
   end
 
   test "rotate returns an error for bad instruction" do
-    assert {:error, message: "bad rotation instruction"} ==
+    assert {:error, message: "bad rotation instruction: H"} ==
              Rover.rotate("H", %Rover{x: 1, y: 2, direction: "W"})
   end
 
@@ -35,5 +35,24 @@ defmodule RoverTest do
   test "move north returns the rover in the new position" do
     assert %Rover{x: 10, y: 11, direction: "N"} ==
              Rover.move(%Rover{x: 10, y: 10, direction: "N"})
+  end
+
+  describe "explore" do
+    test "returns the rover with updated position" do
+      assert %Rover{x: 9, y: 11, direction: "N"} ==
+               Rover.explore(%Rover{x: 10, y: 10, direction: "W"}, %{x: 100, y: 100}, "MRM")
+    end
+
+    test "returns error when out of plateau" do
+      assert {:error, %Rover{x: 10, y: 11, direction: "N"},
+              message: "Rover got out of the plateau."}
+
+      Rover.explore(%Rover{x: 10, y: 10, direction: "W"}, %{x: 100, y: 100}, "RM")
+    end
+
+    test "ignores invalid instructions" do
+      assert %Rover{x: 9, y: 10, direction: "W"} ==
+               Rover.explore(%Rover{x: 10, y: 10, direction: "W"}, %{x: 100, y: 100}, "AAABM")
+    end
   end
 end
